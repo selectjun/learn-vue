14b9f2081f19a20950cf9ba41d3640f11b1c931f
selectjun <selectjun@gmail.com>
2021-07-24 13:24:45 +0900

    Add form control example.

--- test/src/views/Home.vue
+++ test/src/views/Home.vue
@@ -4,6 +4,24 @@
     <input type="text" v-model="input1" />
     <button type="button" @click="getData">Get</button>
     <button type="button" @click="setData">Set</button>
+
+    <select class="form-control" v-model="region" @change="changeRegion">
+      <option :key="index" :value="data.v" v-for="(data, index) in options">{{data.t}}</option>
+    </select>
+
+    <table class="table table-bordered" v-if="tableShow">
+      <tr :key="index" v-for="(data, index) in options">
+        <td>{{data.v}}</td>
+        <td>{{data.t}}</td>
+      </tr>
+    </table>
+
+    <table class="table table-bordered" v-show="tableShow">
+      <tr :key="index" v-for="(data, index) in options">
+        <td>{{data.v}}</td>
+        <td>{{data.t}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -14,7 +32,19 @@
       return {
         title: "selectjun",
         title2: "Seoul",
-        input1: "abcd"
+        input1: "abcd",
+        options: [
+          { v: "S", t: "Seoutl" },
+          { v: "J", t: "Jenu" },
+          { v: "B", t: "Busan" }
+        ],
+        region: "J",
+        tableShow: true
+      }
+    },
+    watch: {
+      input1() {
+        console.log(this.input1);
       }
     },
     methods: {
@@ -23,6 +53,9 @@
       },
       setData() {
         this.input1 =  "123"
+      },
+      changeRegion() {
+        alert(this.region)
       }
     },
     beforeCreate() {
